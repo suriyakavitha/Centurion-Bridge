86c122c919735e639224fc850b4203262193c809
Sudhakar-LF <sudhakar@lfsolutions.net>
2025-08-19 19:07:06 +0530

    Pagination changes

---
+++ TZSmartLockers/wwwroot/js/pagination.js
@@ -0,0 +1,24 @@
+﻿// pagination.js
+export function attachPaginationControls(currentPageRef, rowsPerPageRef, dataArrayRef, renderCallback, prevBtnSelector, nextBtnSelector) {
+
+    if (prevBtnSelector) {
+        document.querySelector(prevBtnSelector)
+            .addEventListener('click', () => {
+                if (currentPageRef.value > 1) {
+                    currentPageRef.value--;
+                    renderCallback();
+                }
+            });
+    }
+
+    if (nextBtnSelector) {
+        document.querySelector(nextBtnSelector)
+            .addEventListener('click', () => {
+                const totalPages = Math.ceil(dataArrayRef.value.length / rowsPerPageRef.value);
+                if (currentPageRef.value < totalPages) {
+                    currentPageRef.value++;
+                    renderCallback();
+                }
+            });
+    }
+}
